0beeb270e82387dfb032650bf84479a5fce8d2ec
Ly Nhu Ngoc <ngoccnlynhu@gmail.com>
2025-12-24 19:58:11 +0700

    Lưu tạm để pull code

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`❌ MongoDB Error: ${error.message}`);
     process.exit(1);
   }
 };
